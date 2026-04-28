32d230491afc3bf649f127eef53f8fb009c86b66
AaronSalazar08 <aaron.salazar.mata@gmail.com>
2026-04-27 21:16:05 -0600

    refactor: remove unused AreaFactory import and related comment

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Database\Factories\AreaFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -11,7 +10,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 #[Fillable(['nombre', 'descripcion', 'color'])]
 class Area extends Model
 {
-    /** @use HasFactory<AreaFactory> */
     use HasFactory;
 
     public function eventos(): BelongsToMany
